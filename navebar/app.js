f75516917bac9bf34dbb80113e558d05af026e2f
Aahad Ali <104500845+Aahad-Ali@users.noreply.github.com>
2022-07-21 21:03:05 +0500

    Add files via upload

---
+++ navebar/app.js
@@ -0,0 +1,10 @@
+function myMenu() {
+    var x = document.getElementById("menu");
+
+    if (x.style.display == "block") {
+        x.style.display = "none";
+    }
+    else {
+        x.style.display = "block";
+    }
+}
